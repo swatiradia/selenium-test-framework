525cc1306d70dc70e5f0464fb0a06ef7239d7779
swatiradia <swatisbhovi@gmail.com>
2024-02-22 15:19:44 -0500

    Adding_base_test_and_global_data

--- src/test/java/Tests/SubmitOrderTest.java
+++ src/test/java/Tests/SubmitOrderTest.java
@@ -1,29 +1,23 @@
+package Tests;
 import PageObjects.*;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import TestComponents.BaseTest;
 import org.testng.Assert;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import java.time.Duration;
+import org.testng.annotations.Test;
+import java.io.IOException;
 
 
 
-public class SubmitOrderTest {
+public class SubmitOrderTest extends BaseTest {
 
-    public static void main(String[] args) {
+    @Test
+        public void SubmitOrder() throws IOException {
 
         String productName ="ZARA COAT 3";
         String countryName = "india";
 
-        WebDriverManager.chromedriver().clearDriverCache().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+/*        All the actions happening in Landing Page are Handled in BaseTest class and the Landing page
+          object is created there and the application is launched using the @BeforeMethod annotation  */
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-/*        All the actions happening in Landing Page */
-
-        LandingPage landingPage = new LandingPage(driver);
-        landingPage.GoToPage();
         ProductCatalogue productCatalogue = landingPage.loginApplication("swati@radia.com", "Swati@radia1");
 
 /*        All the actions happening in Product Catalogue Page */
@@ -47,6 +41,5 @@ public class SubmitOrderTest {
 
         String confirmMessage = confirmationPage.getConfirmationText();
         Assert.assertTrue(confirmMessage.equalsIgnoreCase("Thankyou for the order."));
-        driver.close();
     }
 }
