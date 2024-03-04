b8a142d76982e07f0c58e0c485c98dd1ee32a638
swatiradia <swatisbhovi@gmail.com>
2024-03-04 10:23:41 -0500

    More Code related to cucumber BDD

--- src/test/java/CucumberFiles/TestNGTestRunner.java
+++ src/test/java/CucumberFiles/TestNGTestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 // The below step runs all teh files given in features, maps it to files in glue, monochrome
 // give results in readable format, and generate the reports in html format.
 @CucumberOptions (features = "src/test/java/CucumberFiles", glue = "StepDefinitionFiles",
-monochrome = true /* tags = "@Regression"*/, plugin = {"html:target/cucumber.html"})
+monochrome = true,  tags = "@Regression", plugin = {"html:target/cucumber.html"})
 
 // By default, cucumber will not be able to scan the TestNG assertions/libraries. To create seamless integration
 // we extend AbstractTestNGCucumberTests - helps cucumber understand and TestNg code wherever it's written.
